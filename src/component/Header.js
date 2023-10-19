cad13240ea46ff6c23391607e1ede06c0a3eb049
heeyeon <heeyeon8662@pineone.com>
2023-10-19 15:31:14 +0900

    testBranch

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,7 +2,7 @@ import { Link } from "react--dom";
 const Header = () => {
   return (
     <>
-      <h1>투두리스트</h1>
+      <h1>투두리스트 수정본</h1>
       <Link to="/">홈</Link>
       <Link to="/clear">수행완료</Link>
       <Link to="/unclear">미수행</Link>
